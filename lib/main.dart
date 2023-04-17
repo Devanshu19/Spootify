f37ddc02417f624d764b9935ab3c0fe580bf7611
Devanshu Mishra <devm19102003@gmail.com>
2023-04-17 23:59:33 +0530

    Working Tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:spootify/screens/home.dart';
+import 'package:spootify/screens/search.dart';
+import 'package:spootify/screens/download.dart';
 
 void main(){
   runApp(const Spootify());
@@ -16,22 +19,38 @@ class Spootify extends StatelessWidget {
         brightness: Brightness.dark,
         fontFamily: "Montserrat"
       ),
-      home: HomePage()
+      home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+
+  final tabs = [const Home(), const Search(), const Download()];
+
+  int currentTabIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Spootify Music App"),
+        title: const Text("Spootify Music App"),
       ),
+      body: tabs[currentTabIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        onTap: (tappedItemIndex){
+          currentTabIndex = tappedItemIndex;
+          print(tappedItemIndex);
+          setState(() {});
+        },
+        items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.white), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.search, color: Colors.white), label: "Search"),
           BottomNavigationBarItem(icon: Icon(Icons.download, color: Colors.white), label: "Download"),
---
+++ lib/screens/download.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/material.dart';
+
+class Download extends StatelessWidget {
+  const Download({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Download"
+    );
+  }
+}
